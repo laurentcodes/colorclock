33353a0d6f5066ec8e32d7bc3be0e08e7e1c91d4
laurentcodes <o.sherkes@gmail.com>
2019-05-14 22:48:05 +0100

    Updtaed to Airbnb style guide

--- assets/script.js
+++ assets/script.js
@@ -1,45 +1,46 @@
-/* eslint-disable no-console */
-/* jshint esversion: 6 */
+/* eslint-disable no-multi-assign */
 
 function main() {
-	// Initialize time variable
-	let date, hours, minutes, seconds, body;
+  // Initialize time variable
+  let hours;
+  let minutes;
+  let seconds;
 
-	// Get date Object and store in a variable
-	date = new Date();
+  // Get date Object and store in a variable
+  const date = new Date();
 
-	// Set time variables accordinly
-	hours = date.getHours();
-	minutes = date.getMinutes();
-	seconds = date.getSeconds();
+  // Set time variables accordinly
+  hours = date.getHours();
+  minutes = date.getMinutes();
+  seconds = date.getSeconds();
 
-	// To format time text (00:00:00)
-	if (hours < 10) {
-		// 00-09
-		hours = hours = minutes.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false});
-	}
+  // To format time text (00:00:00)
+  if (hours < 10) {
+    // 00-09
+    hours = hours = minutes.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });
+  }
 
-	if (minutes < 10) {
-		// 00-09
-		minutes = minutes.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false});
-	}
+  if (minutes < 10) {
+    // 00-09
+    minutes = minutes.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });
+  }
 
-	if (seconds < 10) {
-		// 00-09
-		seconds = seconds.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false});
-	}
+  if (seconds < 10) {
+    // 00-09
+    seconds = seconds.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });
+  }
 
-	// Set time and color variables
-	let time = `${hours}:${minutes}:${seconds}`;
-	let color = `#${hours}${minutes}${seconds}`;
+  // Set time and color variables
+  const time = `${hours}:${minutes}:${seconds}`;
+  const color = `#${hours}${minutes}${seconds}`;
 
-	// Update the DOM
-	document.getElementById('text').textContent = time;
-	document.getElementById('text2').textContent = color;
+  // Update the DOM
+  document.getElementById('text').textContent = time;
+  document.getElementById('text2').textContent = color;
 
-	// Change background color using the time
-	body = document.getElementById('main');
-	body.style.background = color;
+  // Change background color using the time
+  const body = document.getElementById('main');
+  body.style.background = color;
 }
 
 window.setInterval(main, 1000);
